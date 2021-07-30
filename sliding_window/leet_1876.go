010c23dda5b290cb9541b053444dc6f3f7fdf2c2
BAN1ce <suhangyang@gmail.com>
2021-07-30 09:15:52 +0800

    :sparkles:

--- sliding_window/leet_1876.go
+++ sliding_window/leet_1876.go
@@ -1,1 +1,29 @@
 package sliding_window
+
+
+// 1876. 长度为三且各字符不同的子字符串
+// https://leetcode-cn.com/problems/substrings-of-size-three-with-distinct-characters/
+func countGoodSubstrings(s string) int {
+
+	left := 0
+	right := 3
+
+	count := 0
+	for ; right <= len(s); {
+		tmp := s[left:right]
+		if isGood(tmp) {
+			count++
+		}
+		left += 1
+		right += 1
+	}
+	return count
+}
+
+func isGood(s string) bool {
+
+	if s[0] == s[1] || s[0] == s[2] || s[1] == s[2] {
+		return false
+	}
+	return true
+}
--- sliding_window/leet_643.go
+++ sliding_window/leet_643.go
@@ -1,1 +1,36 @@
 package sliding_window
+
+// 643. 子数组最大平均数 I
+// https://leetcode-cn.com/problems/maximum-average-subarray-i/
+func findMaxAverage(nums []int, k int) float64 {
+
+	left := 0
+	right := k
+
+	sum := 0
+	for i := 0; i < k; i++ {
+		sum += nums[i]
+	}
+	max := float64(sum) / float64(k)
+
+	for ; right < (len(nums)); {
+
+		var tmp float64
+		if tmp, sum = avg(nums[left], nums[right], sum, k); tmp > max {
+			max = tmp
+		}
+		left += 1
+		right += 1
+
+	}
+
+	return max
+}
+
+// 必须使用减左边的数加右边的数，否则会超时
+func avg(leftValue, rightValue, sum, k int) (float64, int) {
+
+	sum = sum - leftValue + rightValue
+	return float64(sum) / float64(k), sum
+
+}
